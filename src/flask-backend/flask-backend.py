377b1bf36b04d3605a7d236d2eb2c52c8ce04f8d
GrantDaly <gdaly9000@gmail.com>
2022-08-05 12:56:32 -0500

    GTD added a route to flask backend to return json from dummy data file. Is at the route /data
    We can add parameters to this route to select datasets or perform various filtering.

--- src/flask-backend/flask-backend.py
+++ src/flask-backend/flask-backend.py
@@ -1,15 +1,26 @@
+import json
+
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Backend Server for Team ImageNomeR'
+
+
+@app.route("/data")
+def getData():
+    with open('data/dummyFmri1.json') as inFile:
+        data = json.load(inFile)
+        return jsonify(data)
+
     
 # run app from command line
 #  sudo python3 flask-backend.py
 # verified this way on development machine. Will verify on production machine
-# flask --app src/flask-backend/flask-backend.py run --host 0.0.0.0 --port 80
+# sudo flask --app src/flask-backend/flask-backend.py run --host 0.0.0.0 --port 80 --no-debugger
 if(__name__ == '__main__'):
     app.run(host='0.0.0.0', port=80)
 
