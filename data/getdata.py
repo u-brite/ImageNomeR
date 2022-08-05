43627e1baa32fc277e93d1b833a7b37f6fe1303a
Anton Orlichenko <aorliche@gmail.com>
2022-08-05 11:12:31 -0500

    Added FC data

--- data/getdata.py
+++ data/getdata.py
@@ -3,16 +3,18 @@
 import requests
 
 host = 'https://hunimal.org/Hackathon/data'
-files = ['dummyOmics1.json']
+#files = ['dummyOmics1.json']
+files = ['fmri-FC.pkl', 'fmri-FC-slim.pkl']
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
-r = requests.get(f'{host}/{files[0]}', headers=headers)
+for file in files:
+	r = requests.get(f'{host}/{file}', headers=headers)
 
-with open(files[0], 'w') as f:
-	f.write(r.text)
+	with open(files[0], 'w') as f:
+		f.write(r.text)
 
-print(r.status_code)
-if r.status_code != 200:
-	print('Something went wrong')
-else:
-	print('Done')
+	print(r.status_code)
+	if r.status_code != 200:
+		print('Something went wrong')
+	else:
+		print('Done')
